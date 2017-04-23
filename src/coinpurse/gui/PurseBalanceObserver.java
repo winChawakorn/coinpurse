40d45523ef3f01147b9a4108875ab5b214bdbdde
Chawakorn Suphepre <win.chawakorn@gmail.com>
2017-04-23 12:27:03 +0700

    Add Java doc

--- src/coinpurse/gui/PurseBalanceObserver.java
+++ src/coinpurse/gui/PurseBalanceObserver.java
@@ -11,17 +11,28 @@ import javax.swing.SwingConstants;
 
 import coinpurse.Purse;
 
+/**
+ * A UI that shows the presently balance of the purse.
+ * 
+ * @author Chawakorn Suphepre
+ * @version 2017.04.21
+ *
+ */
 public class PurseBalanceObserver extends JFrame implements Observer {
 	private JLabel label;
 
-	// private JScrollPane scrollPane;
-
+	/**
+	 * initialize a new PurseBalanceObserver
+	 */
 	public PurseBalanceObserver() {
 		super("Purse Balance");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		initComponent();
 	}
 
+	/**
+	 * initialize and set the components.
+	 */
 	public void initComponent() {
 		label = new JLabel("", SwingConstants.CENTER);
 		label.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 30));
@@ -30,17 +41,21 @@ public class PurseBalanceObserver extends JFrame implements Observer {
 		this.setLocation(1370, 50);
 	}
 
+	/**
+	 * Start the UI(Make UI visible)
+	 */
 	public void run() {
 		setVisible(true);
 	}
 
+	/**
+	 * Update the UI to show the purse's balance when notified.
+	 */
 	@Override
 	public void update(Observable subject, Object info) {
-		// if (info != null)
-		// System.out.println(">>>>" + info);
+
 		if (subject instanceof Purse) {
 			Purse purse = (Purse) subject;
-			// System.out.println("\nBalance is: " + balance);
 			label.setText(purse.getBalance() + " Baht");
 		}
 	}
--- src/coinpurse/gui/PurseStatusObserver.java
+++ src/coinpurse/gui/PurseStatusObserver.java
@@ -12,16 +12,31 @@ import javax.swing.SwingConstants;
 
 import coinpurse.Purse;
 
+/**
+ * A status bar for the purse shows the amount of the valuable in the
+ * purse(sometimes it shows the status in a word e.g. FULL, EMPTY) and move the
+ * progress bar follow the amount of valuable in the purse.
+ * 
+ * @author Chawakorn Suphepre
+ * @version 2017.04.21
+ *
+ */
 public class PurseStatusObserver extends JFrame implements Observer {
 	private JLabel label;
 	private JProgressBar bar;
 
+	/**
+	 * initialize a new PurseStatusObserver
+	 */
 	public PurseStatusObserver() {
 		super("Purse Status");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		initComponent();
 	}
 
+	/**
+	 * initialize and set the components.
+	 */
 	public void initComponent() {
 		label = new JLabel("", SwingConstants.CENTER);
 		label.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 30));
@@ -32,10 +47,16 @@ public class PurseStatusObserver extends JFrame implements Observer {
 		this.setLocation(1370, 400);
 	}
 
+	/**
+	 * Start the UI(Make UI visible)
+	 */
 	public void run() {
 		setVisible(true);
 	}
 
+	/**
+	 * Update the UI to show the amount status of the purse when notified.
+	 */
 	@Override
 	public void update(Observable subject, Object info) {
 		if (subject instanceof Purse) {
